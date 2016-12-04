cbc35dfc1307e21c05d35d2512a880bfd85cf3cb
bnonni <bryanwnonni@gmail.com>
2016-12-04 15:40:16 -0500

    featured images, taxonomy, reddit

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
-// $('#scroll').jscroll();
-
 //allows all read article buttons to work on click
+$(document).ready(function() {
 for(var i = 0; i < 5; i++) {
-    $('#readArticleButton' + i).on('click', function () {
+    var readArticle = $('#readArticleButton' + i);
+        readArticle.on('click', function () {
         $(this).next().slideToggle(400);
-        var impressions = '#impressionCounter'
-        ;
     });
 }
-//
-// $(document).ready(function() {
-//     $('#popUp .hidden').show();
+
+// $('.scroll').jscroll({
+//     autoTriggerUntil: 3
 // });
+
+// for(var j = 0; j < 5;j++) {
+//     var readCount = $('#readArticleButton1');
+//     var count = 0;
+//     readCount.one('click', function(){
+//         // var impressions = $('#impressionsCounter');
+//         $('#impressionsCounter1').html(++count);
+//     })
 
 
 //API call upon clicking source 1
 $('#Source1').on('click', function(event) {
-    event.preventDefault(); //prevents the page from reloading and puts the source name in the top
-    $('#SourceName').html('Tech Crunch');
-    $('.featuredImage').html('<img src = images/techCrunch.jpg />'); //sets the featured image to TechCrunch logo
-    $('.articleContent h6').html('Latest Tech News'); //sets the taxonomy of the articles
+    event.preventDefault(); //prevents the page from reloading
+    $('#SourceName').html('Tech Crunch'); //adds the source name to the top
+    // $('.featuredImage').html('<img src = images/techCrunch.jpg />'); //sets the featured image to TechCrunch logo
+    // $('.articleContent h6').html('Latest Tech News'); //sets the taxonomy of the articles
 
     var techCrunch = 'https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=fb28fc61b34b465c8312a74a703d953b';
     $.ajax({
@@ -30,7 +36,8 @@ $('#Source1').on('click', function(event) {
                 console.log(data);
                 var article = article1.title;
                 var describe = article1.description;
-                var image = article1.urlToImage
+                var image = article1.urlToImage;
+                var tax = data.sortBy;
                 var idTitle;
                 switch(index){
                     case 1:
@@ -48,8 +55,8 @@ $('#Source1').on('click', function(event) {
                 }
                 $('#title'+idTitle).html(article);
                $('#showContent'+idTitle).html(describe);
-               $('imgOne').attr('src', image);
-               // $('.article .featuredImage').html(image);
+               $('#img'+idTitle).attr('src', image);
+               $('#tax'+idTitle).html(tax);
             });
         }
     });
@@ -59,8 +66,8 @@ $('#Source1').on('click', function(event) {
 $('#Source2').on('click', function(){
     event.preventDefault();
     $('#SourceName').html('ESPN');
-    $('.featuredImage').html('<img src = images/espn.jpg />');
-    $('.articleContent h6').html('Sports News');
+    // $('.featuredImage').html('<img src = images/espn.jpg />');
+    // $('.articleContent h6').html('Sports News');
     var espn =  'https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=fb28fc61b34b465c8312a74a703d953b';
     $.ajax({
         url: espn,
@@ -70,6 +77,8 @@ $('#Source2').on('click', function(){
                 console.log(data);
                 var articleTitle = article1.title;
                 var describe = article1.description;
+                var image = article1.urlToImage;
+                var tax = data.sortBy;
                 var idTitle;
                 switch(index){
                     case 1:
@@ -87,22 +96,19 @@ $('#Source2').on('click', function(){
                 }
                 $('#title'+idTitle).html(articleTitle);
                 $('#showContent'+idTitle).html(describe);
-                // if(articleURL || articleTitle === 'mlb' || 'MLB'){
-                //     $('.articleContent h6').html('Baseball');
-                // }else if (articleURL || articleTitle === 'mma' || 'MMA' || 'boxing'){
-                //     $('.articleContent h6').html('Boxing');}
+                $('#img'+idTitle).attr('src', image);
+                $('#tax'+idTitle).html(tax);
             });
         }
     });
 });
 
-
 //API call upon clicking source 3
 $('#Source3').on('click', function() { //on click event
     event.preventDefault(); //prevents refresh of page and clearing news source
     $('#SourceName').html('Time'); //adds news source Time to News Source window at top
-    $('.featuredImage').html('<img src = images/time.png />'); //feeds in time's logo
-    $('.articleContent h6').html('Latest News');
+    // $('.featuredImage').html('<img src = images/time.png />'); //feeds in time's logo
+    // $('.articleContent h6').html('Latest News');
     var time = 'https://newsapi.org/v1/articles?source=time&sortBy=top&apiKey=fb28fc61b34b465c8312a74a703d953b';
     $.ajax({
         url: time,
@@ -112,6 +118,8 @@ $('#Source3').on('click', function() { //on click event
                 console.log(data);
                 var article = article1.title;
                 var describe = article1.description;
+                var image = article1.urlToImage;
+                var tax = data.sortBy;
                 var idTitle;
                 switch(index){
                     case 1:
@@ -129,10 +137,53 @@ $('#Source3').on('click', function() { //on click event
                 }
                 $('#title'+idTitle).html(article);
                 $('#showContent'+idTitle).html(describe);
+                $('#img'+idTitle).attr('src', image);
+                $('#tax'+idTitle).html(tax);
             });
         }
     });
 });
 
+//API call upon clicking source 4
+    $('#Source4').on('click', function() { //on click event
+        event.preventDefault(); //prevents refresh of page and clearing news source
+        $('#SourceName').html('Reddit'); //adds news source Time to News Source window at top
+        // $('.featuredImage').html('<img src = images/time.png />'); //feeds in time's logo
+        // $('.articleContent h6').html('Latest News');
+        var reddit = 'https://www.reddit.com/r/news/new.json';
+        $.ajax({
+            url: reddit,
+            type: 'GET',
+            success: function (data) {
+                console.log(data);
+                data.children.forEach(function(article1,index){
+                    var article = article1.data.title;
+                    var describe = article1.description;
+                    var image = article1.urlToImage;
+                    var tax = data.sortBy;
+                    var idTitle;
+                    switch(index){
+                        case 1:
+                            idTitle = "One";
+                            break;
+                        case 2:
+                            idTitle = "Two";
+                            break;
+                        case 3:
+                            idTitle = "Three";
+                            break;
+                        case 4:
+                            idTitle = "Four";
+                            break;
+                    }
+                    $('#title'+idTitle).html(article);
+                    $('#showContent'+idTitle).html(describe);
+                    $('#img'+idTitle).attr('src', image);
+                    $('#tax'+idTitle).html(tax);
+                });
+            }
+        });
+    });
+});
 
 
